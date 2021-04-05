f6fecf9c80dc33065607d41bcbdcce015950a618
muhnashir <muhammadnashir9@gmail.com>
2021-04-05 11:46:12 +0700

    menambah api

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -27,46 +27,31 @@
                             <div class="product-pic-zoom">
                                 <img class="product-big-img" :src="gb_default" alt="" />
                             </div>
-                            <div class="product-thumbs">
+                            <div class="product-thumbs" v-if="productDetails.galleries.length > 0">
                                 <carousel class="product-thumbs-track ps-slider" :dots="false" :nav="false">
-                                    <div class="pt" @click="ChangeImage(thumbs[0])" :class="thumbs[0]==gb_default ? 'active':'' ">
-                                        <img src="img/mickey1.jpg" alt="" />
+                                    <div v-for="ss in productDetails.galleries"
+                                    :key="ss.id"
+                                    class="pt" @click="ChangeImage(ss.photo)" :class="ss.photo==gb_default ? 'active':'' ">
+                                        <img :src="ss.photo" alt="" />
                                     </div>
 
-                                    <div class="pt" @click="ChangeImage(thumbs[1])" :class="thumbs[1]==gb_default ? 'active':'' ">
-                                        <img src="img/mickey2.jpg" alt="" />
-                                    </div>
-
-                                    <div class="pt" @click="ChangeImage(thumbs[2])" :class="thumbs[2]==gb_default ? 'active':'' ">
-                                        <img src="img/mickey3.jpg" alt="" />
-                                    </div>
-
-                                    <div class="pt" @click="ChangeImage(thumbs[3])" :class="thumbs[3]==gb_default ? 'active':'' ">
-                                        <img src="img/mickey4.jpg" alt="" />
-                                    </div>
                                 </carousel>
                             </div>
                         </div>
                         <div class="col-lg-6">
                             <div class="product-details text-left">
                                 <div class="pd-title">
-                                    <span>oranges</span>
-                                    <h3>Pure Pineapple</h3>
+                                    <span>{{ productDetails.type}}</span>
+                                    <h3>{{ productDetails.name}}</h3>
                                 </div>
                                 <div class="pd-desc">
-                                    <p>
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, error officia. Rem aperiam laborum voluptatum vel, pariatur modi hic provident eum iure natus quos non a sequi, id accusantium! Autem.
-                                    </p>
-                                    <p>
-                                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam possimus quisquam animi, commodi, nihil voluptate nostrum neque architecto illo officiis doloremque et corrupti cupiditate voluptatibus error illum. Commodi expedita animi nulla aspernatur.
-                                        Id asperiores blanditiis, omnis repudiandae iste inventore cum, quam sint molestiae accusamus voluptates ex tempora illum sit perspiciatis. Nostrum dolor tenetur amet, illo natus magni veniam quia sit nihil dolores.
-                                        Commodi ratione distinctio harum voluptatum velit facilis voluptas animi non laudantium, id dolorem atque perferendis enim ducimus? A exercitationem recusandae aliquam quod. Itaque inventore obcaecati, unde quam
-                                        impedit praesentium veritatis quis beatae ea atque perferendis voluptates velit architecto?
-                                    </p>
-                                    <h4>$495.00</h4>
+                                    <p v-html="productDetails.description"></p>
+                                    <h4>Rp.{{ productDetails.price}}</h4>
                                 </div>
                                 <div class="quantity">
-                                    <router-link to="/cart" class="primary-btn pd-cart">Add To Cart</router-link>
+                                    <!-- <router-link to="/cart"> -->
+                                        <a @click="saveKeranjang(productDetails.id,productDetails.name, productDetails.price, productDetails.galleries[0].photo)" href="#" class="primary-btn pd-cart">Add To Cart</a>
+                                    <!-- </router-link> -->
                                 </div>
                             </div>
                         </div>
@@ -90,6 +75,7 @@ import HeaderCandleshine from '@/components/HeaderCandleshine.vue'
 import FooterCandleshine from '../components/FooterCandleshine.vue'
 import RelatedProduct from '../components/RelatedProduct.vue'
 import carousel from 'vue-owl-carousel'
+import axios from 'axios'
 
 
 export default {
@@ -102,20 +88,54 @@ export default {
   },
   data(){
     return {
-      gb_default: "img/mickey2.jpg",
-      thumbs: [
-        "img/mickey1.jpg",
-        "img/mickey2.jpg",
-        "img/mickey3.jpg",
-        "img/mickey4.jpg",
-      ]
+      gb_default: "",
+      productDetails: [],
+      keranjangUSer : []
     }
   },
   methods: {
     ChangeImage(urlImage){
       this.gb_default = urlImage;
+    },
+    setDataPicture(data){
+        //replace object productDetails dengan data dari API
+        this.productDetails = data;
+
+        //replace value gambar default
+        this.gb_default = data.galleries[0].photo
+
+    },
+    saveKeranjang(idProduct,nameProduct, priceProduct, photoProduct){
+        var productStore={
+            "id" : idProduct,
+            "name" : nameProduct,
+            "price" : priceProduct,
+            "photo" : photoProduct,
+
+        }
+        this.keranjangUSer.push(productStore);
+        const parsed = JSON.stringify(this.keranjangUSer);
+        localStorage.setItem('keranjangUSer', parsed);
     }
-  }
+  },
+  mounted(){
+        if (localStorage.getItem('keranjangUSer')) {
+            try {
+                this.keranjangUSer = JSON.parse(localStorage.getItem('keranjangUSer'));
+            } catch(e) {
+                localStorage.removeItem('keranjangUSer');
+            }
+        }
+        axios
+        .get("http://127.0.0.1:8000/api/products",{
+            params:{
+                id: this.$route.params.id
+            }
+        })
+        .then(res =>(this.setDataPicture(res.data.data)))
+        // eslint-disable-next-line no-console
+        .catch(err=>console.log(err));
+    }
 }
 </script>
 
